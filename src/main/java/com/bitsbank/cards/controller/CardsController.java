08cd30da7fc4251d65a598a74e37c2f76ff40346
Sagar G <sagar.gaigawale@noustek.com>
2023-05-01 19:45:10 +0530

    initial commit

---
+++ src/main/java/com/bitsbank/cards/controller/CardsController.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.bitsbank.cards.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bitsbank.cards.model.Cards;
+import com.bitsbank.cards.model.Customer;
+import com.bitsbank.cards.repository.CardsRepository;
+
+/**
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class CardsController {
+
+	@Autowired
+	private CardsRepository cardsRepository;
+
+	@PostMapping("/myCards")
+	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+		if (cards != null) {
+			return cards;
+		} else {
+			return null;
+		}
+
+	}
+
+}
